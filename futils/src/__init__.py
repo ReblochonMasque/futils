f829a0fed12f6a6782cd98690fff2ae88a521973
ReblochonMasque <trustgold@gmail.com>
2022-05-10 12:25:38 +0800

    add __all__ list of exportable functions

--- futils/src/__init__.py
+++ futils/src/__init__.py
@@ -1,5 +1,15 @@
 from .helpers import *
 
 
+__all__ = [
+    'chunks',
+    'clamp',
+    'count',
+    'make_average',
+    'transpose',
+    'with_previous',
+]
+
+
 def hello():
     print('hello world version 6')
